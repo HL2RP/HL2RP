1eaed90e391b5a304b64fa23f440883044ee7e51
AdRiAnIlloO <adrigibal@gmail.com>
2018-07-15 21:15:53 +0200

    Fix NPCs hitting physics's crash (HL2MP)

--- mp/src/game/server/hl2/hl2_player.cpp
+++ mp/src/game/server/hl2/hl2_player.cpp
@@ -3181,7 +3181,7 @@ void CHL2_Player::ForceDropOfCarriedPhysObjects( CBaseEntity *pOnlyIfHoldingThis
 	ClearUseEntity();
 
 	// Then force the physcannon to drop anything it's holding, if it's our active weapon
-	PhysCannonForceDrop( GetActiveWeapon(), NULL );
+	PhysCannonForceDrop( GetActiveWeapon(), pOnlyIfHoldingThis );
 }
 
 void CHL2_Player::InputForceDropPhysObjects( inputdata_t &data )
