3a86cd56b1fb0841fd73223d106dec46a1ce70a0
Adrianilloo <adrigibal@gmail.com>
2023-02-16 01:31:49 +0100

    [HL2MP] Correct HUD target ID health formula

--- mp/src/game/client/hl2mp/c_hl2mp_player.h
+++ mp/src/game/client/hl2mp/c_hl2mp_player.h
@@ -40,6 +40,7 @@ public:
 	QAngle GetAnimEyeAngles( void ) { return m_angEyeAngles; }
 	Vector GetAttackSpread( CBaseCombatWeapon *pWeapon, CBaseEntity *pTarget = NULL );
 
+	virtual int GetMaxHealth() const { return 100; }
 
 	// Should this object cast shadows?
 	virtual ShadowType_t		ShadowCastType( void );
